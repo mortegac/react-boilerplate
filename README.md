3db65cf0c31e996efa5ac97a71c2477465e25df3
Manuel Ortega <mortega@apgca.cl>
2022-06-08 12:59:15 -0400

    initialize repository

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# ⚛️ React.js Boilerplate
+Boilerplate para proyectos de React.js
+
+------------
+
+### ⚛ React.js v17
+
+
+### 🚀 Para empezar / Get started
+Estas instrucciones le proporcionarán una copia del proyecto en funcionamiento en su máquina local para fines de desarrollo. Siga estos pasos:
+
+------------
+
+```bash
+cd react-boilerplate
+npm install
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+const data = [
+	{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+	{name: 'Page B', uv: 500, pv: 2000, amt: 1400},
+	{name: 'Page C', uv: 800, pv: 1400, amt: 1400}
+];
+
+
+const App = _ => (
+	<>
+		<h2>Gráfico lineal</h2>
+		<LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+			<Line type="monotone" dataKey="uv" stroke="#8884d8" />
+			<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+			<XAxis dataKey="name" />
+			<YAxis />
+			<Tooltip />
+		</LineChart>
+	</>
+);
+
+
+export default App;
